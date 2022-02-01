795a964a1ac8c2b36f7b5bb30750bbfbd826e15b
Wyatt Rose <94425062+Shmoople@users.noreply.github.com>
2022-02-01 12:35:32 -0500

    Reformatted code and added Comments

--- thing.ino
+++ thing.ino
@@ -1,46 +1,58 @@
+/************************************
+|   Author: William Hughes          |
+|   Teacher: Coach L                |
+|   Class: Electrical Engineering   |
+|   Block: B                        |
+|   Program: Tutorial 7 and 8       |
+************************************/
 
-  // William Hughes
-  // Coach L
-  // engineering technology
-  // b block 
+volatile bool buttonValue = false; // boolean variable used to hold the button state
 
-bool buttonVal = false;
+void buttonISR() // interrupt service routine
+{
+    buttonValue = !buttonValue; // flip buttonVal's state
+} // end of interrupt service routine
 
-void setup(){
-  pinMode(A0,INPUT);
-  Serial.begin(9600);
-  pinMode(3,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(13,INPUT);
-  attachInterrupt(digitalPinToInterrupt(13),buttonISR,FALLING);
-}
 
-void buttonISR(){
-  buttonVal = !buttonVal;
-}
-  
- void loop(){
-
-  //digitalWrite(12,HIGH);
+void setup() // built-in setup function
+{
+    /* SETUP INPUT AND OUTPUT PINS */
+    pinMode(A0,INPUT); // A0 is the photoresistor read pin
+    pinMode(13,INPUT); // D13 is the 'momentary contact switch' pin
+    pinMode(3,OUTPUT); // D3 is the led pin 
+    pinMode(12,OUTPUT); // D12 is the piezo pin
     
-    int photoValue = map(analogRead(A0),25,170,0,255);
-    Serial.println(photoValue);
-    Serial.print("buttonVal:");
-    Serial.println(buttonVal);
-    analogWrite(3,photoValue);
-    if(buttonVal){
-      // button is true
-      tone(12, 200);
+
+    /* SETUP ISR PIN */
+    attachInterrupt(digitalPinToInterrupt(13),buttonISR,FALLING); // call 'buttonISR()' when D13 is low
+
+    /* INITIALIZE SERIAL MONITOR */
+    Serial.begin(9600); // baud rate is 9600
+} // end of setup
 
 
-      
-    } else {
-      // buyton is false
-      noTone(12);
+void loop() // built-in loop function
+{
+    /* READ PHOTORESISTOR VALUE */
+    int photoValue = map(analogRead(A0),25,170,0,255); // map the raw analog range to an easier to see brightness range
+    
+    Serial.print("Time: "); // assertment for debugging 
+    Serial.println(millis()/100); // display the amount of time since the program started running (in seconds)
+    Serial.print("photoValue: "); // assertment for debugging
+    Serial.println(photoValue); // display the photoresistor analog input
+    Serial.print("buttonValue: "); // assertment for debugging
+    Serial.println(buttonValue); // display the button sate digital input
+
+    /* UPDATE LED OUTPUT */
+    analogWrite(3,photoValue); // set the brightness of the led to the photoValue
+    
+    /* CHECK FOR BUTTON STATE OUTPUT */
+    if(buttonValue) // if the button is toggled on
+    {
+        tone(12, 200); // play the tune
     }
-    
-    delay(200);
- }
-
-
- 
+    else // if the button is toggled off 
+    {
+        noTone(12); // turn off all music
+    } // end of if-else statement
+} // end of loop
